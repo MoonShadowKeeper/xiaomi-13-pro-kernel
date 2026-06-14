a8eebedec31b2ad798ec1549560dd34a3ea48a8e
MoonShadowKeeper <stasbezsmysly@gmail.com>
2026-06-14 07:22:48 +0300

    Security: Remove hardcoded root password and use environment variables

--- run_ssh.py
+++ run_ssh.py
@@ -3,9 +3,12 @@ import os
 import sys
 import time
 
-ip = "209.74.86.53"
-port = "22022"
-pwd = "123543Moon03!"
+# SECURITY: Do not hardcode root passwords in public repositories!
+# Pass them via environment variables instead.
+ip = os.environ.get("SSH_IP", "209.74.86.53")
+port = os.environ.get("SSH_PORT", "22022")
+pwd = os.environ.get("SSH_PWD")
+
 cmd = sys.argv[1] if len(sys.argv) > 1 else "uname -a"
 
 command = f"ssh -o StrictHostKeyChecking=no root@{ip} -p {port} '{cmd}'"
@@ -25,8 +28,12 @@ else:
             break
         output += data
         if b"password:" in output.lower():
-            os.write(fd, pwd.encode() + b"\n")
-            output = b"" # reset output after password
+            if pwd:
+                os.write(fd, pwd.encode() + b"\n")
+                output = b"" # reset output after password
+            else:
+                print("ERROR: SSH_PWD environment variable is not set!")
+                sys.exit(1)
             
     print(output.decode(errors='ignore'))
     os.waitpid(pid, 0)
